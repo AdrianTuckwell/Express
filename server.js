4cdf645027a6e4b30603309dc6f8e47f96bc37cd
Adrian Tuckwell <adrian.tuckwell@gmail.com>
2016-12-12 11:27:07 +0000

    added restful routes calls

--- server.js
+++ server.js
@@ -1,12 +1,44 @@
 var express = require('express');
+// call express function
 var app = express();
 var path = require('path');
-
+var planets = [{name: 'mars', size: 2093}, {name: 'earth', size: 3089}];
 
 app.use(express.static('client/build'));
 
+//GET
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname + "/client/build/.index.html"));
+})
+
+//NEW
+app.get('/planets/new', function(req, res){
+  res.send("NEW planets routes");
+})
+
+//CREATE
+app.post('/planets', function(req, res){
+  res.send("CREATE planet route");
+})
+
+//SHOW
+app.get('/planets/:id', function(req, res){
+  res.json(planets[req.params.id]);
+})
+
+//EDIT
+app.get('/planets/:id/edit', function(req, res){
+  res.send("EDIT planet route")
+})
+
+//UPDATE
+app.put('/planets/:id', function(req, res){
+  res.send("UPDATE planet route")
+})
+
+//DELETE
+app.delete('/planets/:id', function(req, res){
+  res.send("DELETE planet route, DESTROY planet")
 })
 
 app.listen('3000', function(){
